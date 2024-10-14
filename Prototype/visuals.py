569792a766deee689bc3daf6e08a56c5f6411e46
heimsnik <cmheimvik@gmail.com>
2024-10-15 00:36:08 +0200

    Added all

--- Prototype/visuals.py
+++ Prototype/visuals.py
@@ -46,7 +46,7 @@ def init_bars(config_lst):
     print(config_lst)
     for config in config_lst:
         if config:  # Make sure config is not None (in case there are gaps in IDs)
-            bars.append(PixelStrip(config['number_of_leds'], config['gpio'], LED_FREQ, DMA_CHANNEL, False, 255, config['pwm_channel']))
+            bars.append(PixelStrip(144, 18, LED_FREQ, DMA_CHANNEL, False, 255, 0))
     for i in range(len(bars)):
         bars[i].begin()
         print(f"Bar {i} initialized sucessfully!")
